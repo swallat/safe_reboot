46327e6024d359fdb9ec963c1286084f948b7804
Sebastian Wallat <sebastian.wallat@rub.de>
2020-03-03 14:12:58 +0100

    Added support for ubuntu

--- safe_reboot/safe_reboot.py
+++ safe_reboot/safe_reboot.py
@@ -3,6 +3,8 @@ import subprocess
 import sys
 import click
 import shlex
+import platform
+import pwd
 
 
 def get_all_user_tty_mac():
@@ -21,26 +23,54 @@ def get_all_user_tty_mac():
             users[line[0]].add(line[1])
     return users
 
-
-
 def get_all_user_tty_linux():
     cmd = shlex.split("ps axno user,tty")
+    out = subprocess.check_output(cmd).decode("utf-8")
+    lines = out.strip().split('\n')
+    lines = [x.split() for x in lines[1:]]
+    users = dict()
+    for line in lines:
+        # Only cconsider user ids
+        if int(line[0]) < 1000 or int(line[0]) >= 65530 or line[1] == "?":
+            continue
+        else:
+            if line[0] not in users.keys():
+                users[line[0]] = set()
+            users[line[0]].add(line[1])
+    # Assign names to users:
+    u = dict()
+    for key in users.keys():
+        user_name = pwd.getpwuid(int(key))[0]
+        u[user_name] = users[key]
+    return u
 
-def get_all_user_tty():
-    get_all_user_tty_mac()
+def is_it_safe_to_reboot(show=False):
+    if platform.system() == 'darwin':
+        users = get_all_user_tty_mac()
+    elif platform.system() == 'Linux':
+        users =get_all_user_tty_linux()
+    else:
+        print("OS not supported!: {}".format(os.name), file=sys.stderr)
+        sys.exit(-1)
+    if len(users.keys()) >= 1 or show:
+        print("Following users are active on the system:")
+        for key in users.keys():
+            print("User: {} has {} active ttys.".format(key, len(users[key])))
 
-
-
+        if len(users.keys()) >= 1:
+            print("WARNING: It is not safe to reboot this machine. Other users arecurrently working here!")
+    
+    if len(users.keys()) >= 1:
+        return False
+    else:
+        return True
+    
 @click.command(context_settings=dict(help_option_names=["-h", "--help"]))
 @click.option('--force', '-f', help="Force reboot", default=False, is_flag=True)
 @click.option('--show', '-s', help="Only show active users", default=False, is_flag=True)
 def main(force, show):
     """Console script for safe_reboot."""
-    click.echo("Replace this message by putting your code into "
-               "safe_reboot.cli.main")
-    click.echo("See click documentation at https://click.palletsprojects.com/")
-
-    get_all_user_tty()
+    safe = is_it_safe_to_reboot()
     return 0
 
 
